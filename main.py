4758b5c7406e4b89664f991657ed3126cbaead23
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2022-12-21 12:52:24 +0800

    Update files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import eel
 from os import path
 from yfinance import download
+from tkinter import filedialog
 
 @eel.expose
 def fetch(ticker, startDate, endDate, frequency):
@@ -8,15 +9,15 @@ def fetch(ticker, startDate, endDate, frequency):
         print("\n")
         fetch_data = download(ticker, start = startDate, end = endDate, interval = frequency)
         print(f"\n{fetch_data}")
-        fetch_data.to_excel(f"{Desktop}/{str(ticker)}.xlsx")
+        destination = path.abspath(filedialog.askdirectory())
+        fetch_data.to_excel(f"{destination}/{str(ticker)}.xlsx")
         print("\n操作成功\n")
     except:
         print("\n操作失敗\n")
 
-Desktop = path.normpath(path.expanduser("~/Desktop"))
 root = path.dirname(path.realpath(__file__))
 
-print("*************************************************\n\n                 BlueEyes Engine\n                 Python\n\n*************************************************\n")
+print("*************************************************\n\n                 BlueEyes Engine\n                 ccpl17/shd\n\n*************************************************\n")
 
 eel.init(f'{root}/web')
 eel.start('main.html',shutdown_delay = 1)
